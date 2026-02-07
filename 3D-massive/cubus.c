0fc46de1f53e08ade70ef1eaa29ec68419eccac6
Akram <akrommuhammadyusuf@gmail.com>
2026-02-07 14:04:29 +0700

    Update cubus.c

--- 3D-massive/cubus.c
+++ 3D-massive/cubus.c
@@ -1,77 +1,163 @@
-#include <math.h>
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include <string.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#define CLEAR "cls"
+#define SLEEP(x) Sleep(x)
+#else
 #include <unistd.h>
+#define CLEAR "clear"
+#define SLEEP(x) usleep(x * 1000)
+#endif
 
-float A, B, C;
-float cubeWidth = 20;
-int width = 160, height = 44;
-float zBuffer[160 * 44];
-char buffer[160 * 44];
-int backgroundASCIICode = ' ';
-int distanceFromCam = 100;
-float K1 = 40;
+#define WIDTH 80
+#define HEIGHT 40
+#define CUBE_SIZE 7
 
-float calculateX(int i, int j, int k) {
-    return j * sin(A) * sin(B) * cos(C) - k * cos(A) * sin(B) * cos(C) +
-           j * cos(A) * sin(C) + k * sin(A) * sin(C) + i * cos(B) * cos(C);
-}
+typedef struct {
+    float x, y, z;
+} Point3D;
 
-float calculateY(int i, int j, int k) {
-    return j * cos(A) * cos(C) + k * sin(A) * cos(C) -
-           j * sin(A) * sin(B) * sin(C) + k * cos(A) * sin(B) * sin(C) -
-           i * cos(B) * sin(C);
-}
+typedef struct {
+    float x, y;
+} Point2D;
 
-float calculateZ(int i, int j, int k) {
-    return k * cos(A) * cos(B) - j * sin(A) * cos(B) + i * sin(B);
-}
+char screen[HEIGHT][WIDTH];
+float zbuffer[HEIGHT][WIDTH];
 
-void calculateForSurface(float cubeX, float cubeY, float cubeZ, int ch) {
-    float x = calculateX(cubeX, cubeY, cubeZ);
-    float y = calculateY(cubeX, cubeY, cubeZ);
-    float z = calculateZ(cubeX, cubeY, cubeZ) + distanceFromCam;
-
-    float ooz = 1 / z;
-
-    int xp = (int)(width / 2 + K1 * ooz * x * 2);
-    int yp = (int)(height / 2 + K1 * ooz * y);
-
-    int idx = xp + yp * width;
-    if (idx >= 0 && idx < width * height) {
-        if (ooz > zBuffer[idx]) {
-            zBuffer[idx] = ooz;
-            buffer[idx] = ch;
+void clearScreen() {
+    memset(screen, ' ', sizeof(screen));
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            zbuffer[i][j] = -9999;
         }
     }
+}
+
+Point3D rotateX(Point3D p, float angle) {
+    Point3D r;
+    r.x = p.x;
+    r.y = p.y * cos(angle) - p.z * sin(angle);
+    r.z = p.y * sin(angle) + p.z * cos(angle);
+    return r;
+}
+
+Point3D rotateY(Point3D p, float angle) {
+    Point3D r;
+    r.x = p.x * cos(angle) + p.z * sin(angle);
+    r.y = p.y;
+    r.z = -p.x * sin(angle) + p.z * cos(angle);
+    return r;
+}
+
+Point3D rotateZ(Point3D p, float angle) {
+    Point3D r;
+    r.x = p.x * cos(angle) - p.y * sin(angle);
+    r.y = p.x * sin(angle) + p.y * cos(angle);
+    r.z = p.z;
+    return r;
+}
+
+Point2D project(Point3D p) {
+    Point2D proj;
+    float distance = 30;
+    float factor = distance / (distance + p.z);
+    proj.x = p.x * factor + WIDTH / 2;
+    proj.y = p.y * factor + HEIGHT / 2;
+    return proj;
+}
+
+void drawLine(Point2D p1, Point2D p2, float z, char ch) {
+    int x0 = (int)p1.x, y0 = (int)p1.y;
+    int x1 = (int)p2.x, y1 = (int)p2.y;
+    
+    int dx = abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
+    int dy = -abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
+    int err = dx + dy, e2;
+    
+    while (1) {
+        if (x0 >= 0 && x0 < WIDTH && y0 >= 0 && y0 < HEIGHT) {
+            if (z > zbuffer[y0][x0]) {
+                screen[y0][x0] = ch;
+                zbuffer[y0][x0] = z;
+            }
+        }
+        if (x0 == x1 && y0 == y1) break;
+        e2 = 2 * err;
+        if (e2 >= dy) { err += dy; x0 += sx; }
+        if (e2 <= dx) { err += dx; y0 += sy; }
+    }
+}
+
+void drawCube(float angleX, float angleY, float angleZ) {
+    Point3D vertices[8] = {
+        {-CUBE_SIZE, -CUBE_SIZE, -CUBE_SIZE},
+        {CUBE_SIZE, -CUBE_SIZE, -CUBE_SIZE},
+        {CUBE_SIZE, CUBE_SIZE, -CUBE_SIZE},
+        {-CUBE_SIZE, CUBE_SIZE, -CUBE_SIZE},
+        {-CUBE_SIZE, -CUBE_SIZE, CUBE_SIZE},
+        {CUBE_SIZE, -CUBE_SIZE, CUBE_SIZE},
+        {CUBE_SIZE, CUBE_SIZE, CUBE_SIZE},
+        {-CUBE_SIZE, CUBE_SIZE, CUBE_SIZE}
+    };
+    
+    Point2D projected[8];
+    
+    for (int i = 0; i < 8; i++) {
+        Point3D p = vertices[i];
+        p = rotateX(p, angleX);
+        p = rotateY(p, angleY);
+        p = rotateZ(p, angleZ);
+        projected[i] = project(p);
+    }
+    
+    int edges[12][2] = {
+        {0,1}, {1,2}, {2,3}, {3,0},
+        {4,5}, {5,6}, {6,7}, {7,4},
+        {0,4}, {1,5}, {2,6}, {3,7}
+    };
+    
+    char chars[] = {'#', '@', '*', '+', '=', '-', '.'};
+    
+    for (int i = 0; i < 12; i++) {
+        int idx = i % 7;
+        drawLine(projected[edges[i][0]], projected[edges[i][1]], 
+                 i * 0.1, chars[idx]);
+    }
+}
+
+void render() {
+    system(CLEAR);
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            putchar(screen[i][j]);
+        }
+        putchar('\n');
+    }
+    printf("\n3D ASCII CUBE - Tekan Ctrl+C untuk keluar\n");
 }
 
 int main() {
-    printf("\x1b[2J");
+    float angleX = 0, angleY = 0, angleZ = 0;
+    
+    printf("Memulai animasi 3D ASCII Art...\n");
+    SLEEP(1000);
+    
     while (1) {
-        memset(buffer, backgroundASCIICode, width * height);
-        memset(zBuffer, 0, width * height * 4);
+        clearScreen();
+        drawCube(angleX, angleY, angleZ);
+        render();
         
-        for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += 0.5) {
-            for (float cubeY = -cubeWidth; cubeY < cubeWidth; cubeY += 0.5) {
-                calculateForSurface(cubeX, cubeY, -cubeWidth, '@');
-                calculateForSurface(cubeWidth, cubeY, cubeX, '$');
-                calculateForSurface(-cubeWidth, cubeY, -cubeX, '~');
-                calculateForSurface(-cubeX, cubeY, cubeWidth, '#');
-                calculateForSurface(cubeX, -cubeWidth, -cubeY, ';');
-                calculateForSurface(cubeX, cubeWidth, cubeY, '+');
-            }
-        }
+        angleX += 0.08;
+        angleY += 0.03;
+        angleZ += 0.02;
         
-        printf("\x1b[H");
-        for (int k = 0; k < width * height; k++) {
-            putchar(k % width ? buffer[k] : 10);
-        }
-
-        A += 0.05;
-        B += 0.05;
-        C += 0.01;
-        usleep(8000);
+        SLEEP(30);
     }
+    
     return 0;
 }
