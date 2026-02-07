d8b5d50b71ed9d94adf7dd2210e036333eeaa011
Akram <akrommuhammadyusuf@gmail.com>
2026-02-07 14:09:03 +0700

    Update snakeLinux.c

--- 3D-massive/snakeLinux.c
+++ 3D-massive/snakeLinux.c
@@ -5,3 +5,16 @@
 #include <unistd.h>
 #include <time.h>
 #include <sys/select.h>
+
+#define WIDTH 40
+#define HEIGHT 20
+#define MAX_LENGTH 800
+
+typedef enum { STOP = 0, LEFT, RIGHT, UP, DOWN } Direction;
+
+typedef struct {
+    int x[MAX_LENGTH];
+    int y[MAX_LENGTH];
+    int length;
+    Direction dir;
+} Snake;
